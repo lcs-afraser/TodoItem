09047ba097d5f81367ed353616b3b6445c65286d
Alistair Fraser <afraser@lcs.on.ca>
2023-04-04 09:13:20 -0400

    Initial Commit

---
+++ TodoItem/TodoItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoItemApp.swift
+//  TodoItem
+//
+//  Created by Alistair Fraser on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct TodoItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
