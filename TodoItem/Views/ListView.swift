9a590aac1371ceeee464a10e16868c65f620ea3b
Alistair Fraser <afraser@lcs.on.ca>
2023-04-04 09:16:22 -0400

    Deleted content view and made ListView

---
+++ TodoItem/Views/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  TodoItem
+//
+//  Created by Alistair Fraser on 2023-04-04.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
