dc049819cfbf954fec790ae87d6fccaa0d51a633
Alistair Fraser <afraser@lcs.on.ca>
2023-04-13 11:31:14 -0400

    added search function to my app

---
+++ TodoItem/Views/ListItemsView.swift
@@ -0,0 +1,85 @@
+//
+//  ListItemsView.swift
+//  TodoItem
+//
+//  Created by Alistair Fraser on 2023-04-13.
+//
+import Blackbird
+import SwiftUI
+
+struct ListItemsView: View {
+    
+    //MARK: Stored Properties
+    
+    //Need to query database
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
+    
+    //The list of items to be completed
+    @BlackbirdLiveModels var todoItems: Blackbird.LiveResults<TodoItem>
+    
+    var body: some View {
+        List {
+            ForEach(todoItems.results) { currentItem in
+                
+                Label(title: { Text(currentItem.description)}, icon: {
+                    if currentItem.completed == true {
+                        Image(systemName: "checkmark.circle")
+                    } else {
+                        Image(systemName: "circle")
+                    }
+                })
+                .onTapGesture {
+                    Task {
+                        try await db!.transaction { core in
+                            //Change the status for this person to the opposite of its current value
+                            try core.query("UPDATE TodoItem SET completed = (?) WHERE id = (?)",
+                                           !currentItem.completed,
+                                           currentItem.id)
+                        }
+                    }
+                }
+            }
+            .onDelete(perform: removeRows)
+            
+        }
+    }
+    //MARK: initializers
+    init(filteredOn searchText: String) {
+    
+    //Initialize the live model
+    _todoItems = BlackbirdLiveModels({ db in
+        try await TodoItem.read(from: db,
+                                sqlWhere: "description LIKE ?", "%\(searchText)%")
+    })
+    
+}
+    //MARK: Functions
+    func removeRows(at offsets: IndexSet) {
+        Task {
+            try await db!.transaction { core in
+                
+                //Get the ID of the item to be deleted
+                var idList = ""
+                for offset in offsets {
+                    idList += "\(todoItems.results[offset].id),"
+                }
+                //Remove the final comma
+                print(idList)
+                idList.removeLast()
+                print(idList)
+                //Delete the rows from the database
+                try core.query("DELETE FROM TodoItem WHERE id IN (?)", idList)
+                
+            }
+        }
+    }
+}
+
+struct ListItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListItemsView(filteredOn: "")
+        //Make the database avaliable to all other views through the environment
+            .environment(\.blackbirdDatabase, AppDatabase.instance)
+
+    }
+}
--- TodoItem/Views/ListView.swift
+++ TodoItem/Views/ListView.swift
@@ -14,13 +14,11 @@ struct ListView: View {
     @Environment(\.blackbirdDatabase) var db:
     Blackbird.Database?
     
-    //The list of items to be completed
-    @BlackbirdLiveModels({db in
-        try await TodoItem.read(from: db)
-    }) var todoItems
-    
     //The items currently being added
     @State var newItemDescription: String = ""
+    
+    //The current search text
+    @State var searchText = ""
     
     //MARK: Computed Properties
     var body: some View {
@@ -43,56 +41,14 @@ struct ListView: View {
                 }
                 .padding(20)
                 
-                List {
-                    ForEach(todoItems.results) { currentItem in
-                        
-                        Label(title: { Text(currentItem.description)}, icon: {
-                            if currentItem.completed == true {
-                                Image(systemName: "checkmark.circle")
-                            } else {
-                                Image(systemName: "circle")
-                            }
-                        })
-                        .onTapGesture {
-                            Task {
-                                try await db!.transaction { core in
-                                    //Change the status for this person to the opposite of its current value
-                                    try core.query("UPDATE TodoItem SET completed = (?) WHERE id = (?)",
-                                                   !currentItem.completed,
-                                                   currentItem.id)
-                                }
-                            }
-                        }
-                    }
-                    .onDelete(perform: removeRows)
-
-                }
+                ListItemsView(filteredOn: searchText)
+                
+                .searchable(text: $searchText)
             }
             .navigationTitle("To-Do List")
         }
         
         
-    }
-    
-    //MARK: Functions
-    func removeRows(at offsets: IndexSet) {
-        Task {
-            try await db!.transaction { core in
-                
-                //Get the ID of the item to be deleted
-                var idList = ""
-                for offset in offsets {
-                    idList += "\(todoItems.results[offset].id),"
-                }
-                //Remove the final comma
-                print(idList)
-                idList.removeLast()
-                print(idList)
-                //Delete the rows from the database
-                try core.query("DELETE FROM TodoItem WHERE id IN (?)", idList)
-                
-            }
-        }
     }
 }
 struct ListView_Previews: PreviewProvider {
